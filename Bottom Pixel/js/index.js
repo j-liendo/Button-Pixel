40c87d77a9a9e4b23103197ea806edf88a3a3b48
josh1502-bit <72353245+josh1502-bit@users.noreply.github.com>
2021-06-21 20:41:45 -0400

    Pix

--- Bottom Pixel/js/index.js
+++
@@ -1,8 +0,0 @@
-const count = document.getElementById("count");
-
-i = 0
-
-function func() {
-    i += 1
-    count.innerHTML = i;
-}
---
+++ js/index.js
@@ -0,0 +1,8 @@
+const count = document.getElementById("count");
+
+i = 0
+
+function func() {
+    i += 1
+    count.innerHTML = i;
+}
