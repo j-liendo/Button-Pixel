aa0328c9c1d989328877e6945082fec14605a412
josh1502-bit <72353245+josh1502-bit@users.noreply.github.com>
2021-06-23 19:07:44 -0400

    sound add

--- js/index.js
+++ js/index.js
@@ -1,8 +1,10 @@
 const count = document.getElementById("count");
+const sfx = new Audio("../sound/Blip_Select18.wav");
 
-i = 0
+i = 0;
 
 function func() {
     i += 1
     count.innerHTML = i;
+    sfx.play();
 }
